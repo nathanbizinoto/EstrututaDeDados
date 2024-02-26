659728c7b9fe0914e5f334ab5f0eeb6fe4f82259
NATHAN BIZINOTO DE OLIVEIRA <1091392323023@fatec-srv.local>
2024-02-26 19:56:20 -0300

    Primeiro commit

---
+++ 02_listas.py
@@ -0,0 +1,95 @@
+# LISTAS são uma estrututa de dados nativa da
+# linguagem Python. Elas permitem que vários
+# valores possam ser associados a uma única 
+# varíavel
+
+#Lista de strings
+legumes = ["batata", "cenoura", "beterraba", "mandioquinha", "batata doce"]
+
+#Lista de números
+nums = [3, 10, -7, 12.8, -0.5]
+
+#Lista com valores de vários tipos
+mistureba = ["Joaquim", 37, 1.81, 88, True]
+
+## OPERAÇÕES SOBRE LISTAS ##
+
+# 1) PERCURSO
+# Percorrer uma lista significa visitar cada um de 
+# seus itens e fazer algo com ele. No exemplo abaixo,
+# vamos dar um print() em cada um dos legumes da lista
+for leg in legumes:
+        print(leg)
+
+# Traço separador
+print("-" * 60)        
+        
+# 3) INSERÇÃO DE UM NOVO ELEMENTO NA POSIÇÃO ESPECIFICADA: insert()
+#    Parâmetros:
+#    1° ~> a posição onde será feita a inserção
+#    2° ~> O novo elemento a ser inserido
+
+#Inserindo um novo elemento na QUARTA posiçao
+legumes.insert(3, "tomate")
+print(legumes) 
+
+# Traço separador
+print("-" * 60)
+
+#4) ACESSANDO VALORES, INFORMANDO A RESPECTIVA POSIÇÃO
+print("Elemento na QUINTA posição: ", legumes[4])
+print("Elemento na PRIMEIRA posição: ", legumes[1])
+print("Elemento na ÚLTIMA posição: ", legumes[-1])
+print("Elemento na PENÚLTIMA posição: ", legumes[-2])
+
+# Traço separador
+print("-" * 60)
+
+#5) SUBTITUINDO VALORES EXISTENTES
+
+print("ANTES: ", legumes)
+
+#Substituindo o valor na posição  3 (QUARTA posição)
+legumes[3] = "vagem"
+#Substituindo o valor na posição  0 (PRIMEIRA posição)
+legumes[0] = "nabo"
+#Substituindo o valor na posição -1 (ÚLTIMA posição)
+legumes[-1] = "inhame"
+
+print("DEPOIS", legumes)
+
+#Traço separador
+print("-" * 60)
+
+# 6) DETERMINANDO A QUANTIDADE
+print("Quantidade de elementos da lista de legumes: ", len(legumes))
+print("Quantidade de elementos da lista de números: ",len(legumes) )
+
+# Traço separador
+print("-" * 60)
+
+# 7) REMOVENDO O ÚLTIMO ELEMENTO B DE UMA LISTA: pop() (sem parãmetro)
+print("ANTES:", legumes)
+removido = legumes.pop()
+print("Valor removido: ", removido)
+print("DEPOIS: ", legumes)
+
+#Traço separador
+print("-" * 60)
+
+# 8) REMOVENDO UM ELEMENTO POR SUA POSICAO: pop() com parâmetro
+removido = legumes.pop(3)       #Remove o elemento da posição 3
+print(legumes)
+
+removido = legumes.pop(0)       #Remove o primeiro elemento
+print("Valor removido da primeira posição: ", removido)
+print(legumes)
+
+# 9) REMOVENDO UM ELEMENTO POR SEU VALOR: remove()
+removido = legumes.remove("mandioquinha")
+print("Valor removido:", removido)
+print(legumes)
+
+# Traços separador
+print("-" * 60)
+
