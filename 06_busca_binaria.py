b71e5dbed6b0a1506b95252e360edf50f5772ee0
NATHAN BIZINOTO DE OLIVEIRA <1091392323023@fatec-srv.local>
2024-03-04 20:22:27 -0300

    Update

--- 06_busca_binaria.py
+++ 06_busca_binaria.py
@@ -1,3 +1,5 @@
+comps = 0           # Contador de comparações
+
 def busca_binaria(lista, val):
     """
     ALGORITMO DE BUSCA BINÁRIA
@@ -9,6 +11,12 @@ def busca_binaria(lista, val):
     feitas até que se encontre o valor de busca ou que
     conclui que o valor de busca não existe na lista
     """
+
+    # Declaramos que queremos usar a variável global comps
+    # inicializada na linha 1
+
+    global comp
+    comps = 0
 
     ini = 0                # Posição inicial da lista
     fim = len(lista) - 1   # Posição final da lista
@@ -23,12 +31,14 @@ def busca_binaria(lista, val):
         # busca foi encontrado
 
         if val == lista[meio]:
+            comps += 1
             return meio
     
         # Senão, se o valor de busca é menor do que o
         # valor do meio, reinicia a busca na metade esquerda
         # da (sub)lista
         elif val < lista[meio]:
+            comps += 2
             fim = meio - 1
 
         # Por fim, se o valor de busca é MAIOR do que o 
@@ -42,3 +52,35 @@ def busca_binaria(lista, val):
     # o valor de busca não existe na lista
     return -1
     #######################################################################
+
+import sys
+sys.dont_write_bytecode = True
+
+from time import time 
+
+# TESTES COM NOMES
+from nomes_completos import nomes
+
+hora_ini = time()
+resultado1 = busca_sequencial(nomes, "EDSON PEREIRA")
+hora_fim = time()
+print(f"EDSON PEREIRA encontrado na posição {resultado1}")
+print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n")
+
+hora_ini = time()
+resultado2 = busca_sequencial(nomes, "MARIA FERREIRA")
+hora_fim = time()
+print(f"MARIA FERREIRA encontrado na posição {resultado2}")
+print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n")
+
+hora_ini = time()
+resultado3 = busca_sequencial(nomes, "VALDIR SILVA")
+hora_fim = time()
+print(f"VALDIR SILVA encontrado na posição {resultado3}")
+print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n")
+
+hora_ini = time()
+resultado4 = busca_sequencial(nomes, "GILCINEIA GARCIA")
+hora_fim = time()
+print(f"GILCINEIA GARCIA encontrado na posição {resultado4}")
+print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms\n")
